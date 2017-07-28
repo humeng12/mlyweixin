3d3803f0c2a3765d7ef8b168930d08a81eae2662
humeng12 <h13273809702@163.com>
2017-07-28 10:30:20 +0800

    1
    代码上传

---
+++ common/common.js
@@ -0,0 +1,25 @@
+
+function set(key,value){
+    var curTime = new Date().getTime();
+    localStorage.setItem(key,JSON.stringify({data:value,time:curTime}));
+}
+
+
+function get(key,exp){
+	
+    var data = localStorage.getItem(key);
+    var dataObj = JSON.parse(data);
+    
+    if(exp != null)
+    {
+    	if (new Date().getTime() - dataObj.time > exp) {
+    		return null;
+	    }else{
+	        var dataObjDatatoJson = JSON.parse(dataObj.data);
+	        return dataObjDatatoJson;
+	    }	
+    }else{
+    	var dataObjDatatoJson = JSON.parse(dataObj.data);
+	    return dataObjDatatoJson;
+    }
+}
