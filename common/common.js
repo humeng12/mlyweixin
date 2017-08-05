70e3badf92396c75dbf0b716ccbd803fbd515e37
humeng12 <h13273809702@163.com>
2017-08-05 10:20:37 +0800

    11
    11

--- common/common.js
+++ common/common.js
@@ -25,3 +25,15 @@ function getInfo(key,exp){
 	    return dataObjDatatoJson;
     }
 }
+
+
+
+function getQueryStringByName(name) {
+
+	var result = location.search.match(new RegExp("[\?\&]" + name + "=([^\&]+)", "i"));
+
+	if(result == null || result.length < 1) {
+		return "";
+	}
+	return result[1];
+}
